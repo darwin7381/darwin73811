6232157f2c38c976da1ada2dd70a18d691e22c78
2 <darwin7381987654@gmail.com>
2017-09-06 22:49:04 +0800

    remove redundant dir

--- Desktop/CryptoCurrency/info_station/eth_js/sample.js
+++
@@ -1,6 +0,0 @@
-var Web3 = require('web3');
-// create an instance of web3 using the HTTP provider.
-// NOTE in mist web3 is already available, so check first if its available before instantiating
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-var version = web3.version.node;
-console.log(version); // "Mist/v0.9.3/darwin/go1.4.1"
